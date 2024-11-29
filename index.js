e93c487e5b0b48acc4a119f84e16cbda00774b1e
brianwu <fslikeyou1033@yahoo.com>
2024-11-29 15:49:04 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import fs from 'fs';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+// app.get('/artworks', (req, res) => {
+//     const data = JSON.parse(fs.readFileSync(DATA_PATH));
+
+//     data.forEach((artwork) => {
+//         artwork.alt_text = artwork.thumbnail?.alt_text ?? null;
+//         delete artwork.thumbnail;
+//     })
+
+//     res.status(200).json(data);
+// })
+
+app.listen(PORT, () => {
+    console.log(`Running Magnum Opus API on http://localhost:${PORT}...`);
+});

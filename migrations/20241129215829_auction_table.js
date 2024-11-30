977d0535d6bcc635dc09b371e22c77be59463758
brianwu <fslikeyou1033@yahoo.com>
2024-11-29 20:55:23 -0500

    auction table migration

--- migrations/20241129215829_auction_table.js
+++ migrations/20241129215829_auction_table.js
@@ -3,7 +3,26 @@
  * @returns { Promise<void> }
  */
 export function up(knex) {
-    return knex.schema
+    return knex.schema.createTable("auction", (table) => {
+        table.increments("id").primary();
+        table
+            .integer("artwork_id")
+            .unsigned()
+            .references("artwork.id")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        table
+            .integer("owner_id")
+            .unsigned()
+            .references("user.id")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        table.timestamp("open_at").notNullable();
+        table.timestamp("close_at").notNullable();
+        table.integer("ask_price").notNullable();
+        table.integer("leading_bid_price").notNullable;
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+    });
 };
 
 /**
@@ -11,5 +30,5 @@ export function up(knex) {
  * @returns { Promise<void> }
  */
 export function down(knex) {
-    return knex.schema
+    return knex.schema.dropTable("auction");
 };
--- migrations/20241129215848_user_table.js
+++ migrations/20241129215848_user_table.js
@@ -17,5 +17,5 @@ export function up(knex) {
  * @returns { Promise<void> }
  */
 export function down(knex) {
-    return knex.schema
+    return knex.schema.dropTable("user")
 };

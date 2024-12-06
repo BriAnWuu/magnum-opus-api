eeaa6e32b258ded5873718664a9985d8ccb139c1
brianwu <fslikeyou1033@yahoo.com>
2024-12-06 18:05:16 -0500

    add fied to auction api

--- controllers/auction-controller.js
+++ controllers/auction-controller.js
@@ -5,15 +5,26 @@ const knex = initKnex(configuration);
 const get = async (req, res) => {
     try {
         const auction = await knex("auction")
+            .join("artwork", "auction.artwork_id", "=", "artwork.id")
             .select(
-                "id",
+                "auction.id",
                 "artwork_id",
                 "owner_id",
                 "open_at",
                 "close_at",
                 "ask_price",
                 "leading_bid_price",
-                "watchers"
+                "watchers",
+                "title",
+                "date_start",
+                "date_end", 
+                "place_of_origin",
+                "description",
+                "dimensions",
+                "medium_display",
+                "provenance_text",
+                "artist_title",
+                "alt_text"
             )
             .where({ artwork_id: req.params.artworkId })
             .first();

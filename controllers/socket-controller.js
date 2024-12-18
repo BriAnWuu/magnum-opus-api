3e5f1e1fa367f9d44f1e45b0b9b3f68810f537b3
brianwu <fslikeyou1033@yahoo.com>
2024-12-18 16:48:03 -0500

    fix userApi watcher id type / add broadcast on newBid

--- controllers/socket-controller.js
+++ controllers/socket-controller.js
@@ -1,7 +1,11 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+
+
 let connectedUsers = [];
 
 const addUser = (userId, socketId) => {
-    !connectedUsers.some((user) => user.userId === userId) &&
+    !connectedUsers.some((user) => user.socketId === socketId) &&
         connectedUsers.push({ userId, socketId });
 }
 
@@ -15,19 +19,62 @@ const getUser = (userId) => {
     return connectedUsers.find((user) => user.userId === userId);
 }
 
+const getWatcherList = async (auctionId) => {
+    const knex = initKnex(configuration);
+    try {
+        const auction = await knex("auction")
+            .select("id", "watchers")
+            .where({ id: auctionId })
+            .first();
+
+        auction.watchers = JSON.parse(auction.watchers);
+
+        return auction
+
+    } catch (error) {
+        console.error('Database query failed:', error);
+        throw error;
+    }
+}
+
+const newBidBroadcast = async (io, socket, auctionId) => {
+    try {
+        const auction = await getWatcherList(auctionId);
+
+        // match watchers and connected sockets
+        auction.watchers.forEach((watcherId) => {
+            connectedUsers.filter(user =>
+                user.userId === watcherId
+            ).forEach((user => {
+                socket.to(user.socketId).emit("newBidBroadcast", {
+                    success: true, 
+                    auction
+                });
+            }));
+        });
+
+    } catch (error) {
+        console.error(error);
+        io.emit("newBidBroadcast", { 
+            success: false, 
+            error: "Failed to get watchers" 
+        });
+    }
+}
+
 const socketOnConnect = (io) => {
     io.on("connection", (socket) => {
+        console.log(`User connected ${socket.id}`)
         
         socket.on("addNewUser", (userId) => {
             addUser(userId, socket.id);
             console.log(connectedUsers);
         });
 
-
-        setTimeout(() => {
-            io.emit('test_broadcast', { message: "testing" });
-            console.log('test message is sent to', socket.id)
-        }, 5000);
+        socket.on("onNewBid", (auctionId) => {
+            // console.log("new bid!!")
+            newBidBroadcast(io, socket, auctionId)
+        });
 
         socket.on("disconnect", () => {
             removeUser(socket.id);
